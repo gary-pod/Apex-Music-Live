bec4f58b746e8f83c0002c411f8a54676608673d
Gary Miskimmons <gary_1990@live.co.uk>
2018-07-19 19:48:21 +0100

    local instagram access token

--- public/assets/js/master.js
+++ public/assets/js/master.js
@@ -1,15 +1,15 @@
-// var feed = new Instafeed({
-//     get: 'user',
-//     userId: 269832113,
-//     accessToken: '269832113.1677ed0.132cf9fc530440f985fcb2d6fc91bbb4',
-//     resolution: 'standard_resolution',
-//     limit: 20,
-//     resolution: 'thumbnail',
-//     template: '<div class="column column-block"><img src="{{image}}" class="thumbnail" alt="" /></div>'
-// });
+var feed = new Instafeed({
+    get: 'user',
+    userId: 483723091,
+    accessToken: '483723091.4772df1.7237471115b440df8819214fcaa914b8',
+    resolution: 'standard_resolution',
+    limit: 20,
+    resolution: 'thumbnail',
+    template: '<div class="column column-block"><img src="{{image}}" class="thumbnail" alt="" /></div>'
+});
   
 if(window.location.pathname == '/' || window.location.pathname == '/apex-studio') {
-  //feed.run();
+  feed.run();
 } 
 
 
