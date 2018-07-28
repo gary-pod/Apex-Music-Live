56afa8ea75155029bf4a0625c8def858a705c74d
Gary Miskimmons <gary_1990@live.co.uk>
2018-07-28 17:08:06 +0100

    Compatabilty Testing

--- public/assets/js/master.js
+++ public/assets/js/master.js
@@ -16,7 +16,7 @@ if(window.location.pathname == '/' || window.location.pathname == '/apex-studio'
 jQuery(function($) {
   // Asynchronously Load the map API 
   var script = document.createElement('script');
-  script.src = "//maps.googleapis.com/maps/api/js?key=AIzaSyD6otD5di-JKgq0GSvxSm7Ytz6WnEu1O5k&callback=initialize";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD6otD5di-JKgq0GSvxSm7Ytz6WnEu1O5k&callback=initialize";
   document.body.appendChild(script);
 });
 
@@ -26,38 +26,40 @@ function initialize() {
   var mapOptions = {
       mapTypeId: 'roadmap'
   };
-                  
-  // Display a map on the page
-  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  map.setTilt(45);
+
+  if(window.location.pathname == '/contact') {
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
       
-  // Multiple Markers
-  var markers = [
-      ['APEX STUDIO, Unit 3, Derrycrin road  BT80 0HJ Cookstown', 54.643711,-6.5613988],
-      ['APEX WORKSHOP, 14 Sullenboy park BT80 8HPCookstown', 54.6401879,-6.7528764]
-  ];
-      
-  // Display multiple markers on a map
-  var infoWindow = new google.maps.InfoWindow(), marker, i;
+    // Multiple Markers
+    var markers = [
+        ['APEX STUDIO, Unit 3, Derrycrin road  BT80 0HJ Cookstown', 54.643711,-6.5613988],
+        ['APEX WORKSHOP, 14 Sullenboy park BT80 8HPCookstown', 54.6401879,-6.7528764]
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
   
-  // Loop through our array of markers & place each one on the map  
-  for( i = 0; i < markers.length; i++ ) {
-      var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-      bounds.extend(position);
-      marker = new google.maps.Marker({
-          position: position,
-          map: map,
-          title: markers[i][0]
-      });
-      
-      // Automatically center the map fitting all markers on the screen
-      map.fitBounds(bounds);
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(12);
+        google.maps.event.removeListener(boundsListener);
+    });
   }
 
-  // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-  var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-      this.setZoom(12);
-      google.maps.event.removeListener(boundsListener);
-  });
   
 }
